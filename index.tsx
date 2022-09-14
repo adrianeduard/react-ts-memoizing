8b25cb89bfc0d0344409dc69203da7cb245b205a
adrianeduard <adrian.eduard@gmail.com>
2022-09-15 00:59:09 +0300

    2 files modified, 3 added

--- index.tsx
+++ index.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import { rootStore } from './store/rootStore';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={rootStore}>
+      <App />
+    </Provider>
   </StrictMode>
 );
